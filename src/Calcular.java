9b90af616a96bc3bb552d5fdd1f424433f034031
Jeter Scheer <jeterscheer64@gmail.com>
2017-08-19 23:39:32 -0300

    Commit inicial

---
+++ src/Calcular.java
@@ -0,0 +1,8 @@
+
+public class Calcular {
+
+    public static void main(String[] args) {
+        int resultado = 1 + 1;
+        System.out.println(String.format("Resultado: %s.", resultado));
+    }
+}
